c2cc0ebefcccec99b9e1d337d6b21779cc155e8b
Luiz Willian <luizwilliasoares930@gmail.com>
2026-07-17 23:56:01 -0300

    Corrige a formatação do código e ajusta a mensagem de saída da soma total dos pares

--- Desafio3.py
+++ Desafio3.py
@@ -1,10 +1,10 @@
 soma_pares = 0
 
-for i in range(1, 21):   
+for i in range(1, 21):
     if i % 2 == 1:
-         print(f"{i} - Impar")
+        print(f"{i} - Impar")    
     else:
         print(f"{i} - Par")
         soma_pares += i
 
-print(f"Soma total do pares é: {soma_pares}")
+print(f"Soma total dos pares é: {soma_pares}")
